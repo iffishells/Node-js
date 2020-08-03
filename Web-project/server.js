c25344dc2f73d8c35ad44d703493b4a590b660ac
iffishells <iffishells@gmail.com>
2020-08-03 15:59:44 +0500

    updated

--- Web-project/server.js
+++ Web-project/server.js
@@ -1,4 +1,3 @@
-
 const express = require("express");
 const app = express();
 const path = require("path")
@@ -7,23 +6,22 @@ const ejs = require('ejs');
 // const ejsLint = require('ejs-lint');
 
 app.use(express.static(path.join(__dirname, 'public'))); //static files
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"))
+
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs") // for setting to ejs tempalte
 
-
-
-app.get("/" , function (req , resp) {
-  resp.send("BisMillah")
+app.get("/", function(req, resp) {
+  resp.render("index")
 
 });
-
 
 app.post("/", function(req, resp) {
   console.log("post method has been called");
   resp.send("Okies")
 });
-
 
 app.listen(3000, function(req, resp) {
   console.log("your's  is listen at 3000 ports");
